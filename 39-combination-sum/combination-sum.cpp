f1caf7b9e4bc117ba8682bbb5e801d5259c81f78
gudiDedeepya <gudidedeepya@gmail.com>
2025-12-12 12:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.23%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,27 @@
 
 
 class Solution {
-    
 public:
-    void generate(const vector<int>& candidates, int target,
-                  vector<vector<int>>& combo, int index,
-                  vector<int>& ls, int sum) {
-        if (sum == target) {
-            combo.push_back(ls);
-            return;
+void generate( vector<int>&candidates,vector<vector<int>>&ans,vector<int>&ls,int target,int index){
+        if(index==candidates.size()){
+            if(target==0){
+                ans.push_back(ls);
+             
+            }
+               return;
         }
-        if (index == (int)candidates.size() || sum > target) return;
-        ls.push_back(candidates[index]);
-        generate(candidates, target, combo, index, ls, sum + candidates[index]);
-        ls.pop_back();
-        generate(candidates, target, combo, index + 1, ls, sum);
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> combo;
-        vector<int> ls;
-        generate(candidates, target, combo, 0, ls, 0);
-        return combo;
-    }
+            if(candidates[index]<=target){
+                ls.push_back(candidates[index]);
+                 generate(candidates,ans,ls,target-candidates[index],index);
+                 ls.pop_back();
+            }
+                  generate(candidates,ans,ls,target,index+1);
+            
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+              vector<vector<int>>ans;
+              vector<int>ls;
+              generate(candidates,ans,ls,target,0);
+              return ans;
+    } 
 };
