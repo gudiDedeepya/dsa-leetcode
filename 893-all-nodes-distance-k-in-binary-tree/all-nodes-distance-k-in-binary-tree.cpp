1be0c8984fb6aabd3565435a0d1d88eb3de7a1e9
gudiDedeepya <gudidedeepya@gmail.com>
2026-05-25 13:42:42 +0530

    Time: 5 ms (45.35%) | Memory: 15.7 MB (22.07%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+void markParents(TreeNode* root,
+                 unordered_map<TreeNode*, TreeNode*>& parents) {
+
+    queue<TreeNode*> q;
+
+    q.push(root);
+
+    while(!q.empty()) {
+
+        TreeNode* current = q.front();
+        q.pop();
+
+        if(current->left) {
+
+            parents[current->left] = current;
+            q.push(current->left);
+        }
+
+        if(current->right) {
+
+            parents[current->right] = current;
+            q.push(current->right);
+        }
+    }
+}
+void dis(TreeNode*root,TreeNode* target, int k, unordered_map<TreeNode*, TreeNode*>parents,vector<int>&ans){
+        queue<pair<TreeNode*,int>>q;
+        q.push({target,0});
+        unordered_map<TreeNode*, bool> visited;
+        visited[target]=true;
+        while(!q.empty()){
+            TreeNode* node=q.front().first;
+            int cnt=q.front().second;
+            q.pop();
+            if(node->left&&!visited[node->left]){
+                visited[node->left]=true;
+                if(cnt+1==k) ans.push_back(node->left->val);
+                else q.push({node->left,cnt+1});
+            }
+
+            if(node->right&&!visited[node->right]) {
+                visited[node->right]=true;
+                if(cnt+1==k) ans.push_back(node->right->val);
+                else q.push({node->right, cnt+1});
+            }
+            if(parents[node]&&!visited[parents[node]]){
+                visited[parents[node]]=true;
+                if(cnt+1==k) ans.push_back(parents[node]->val);
+                else q.push({parents[node], cnt+1});
+            }
+
+ }
+
+
+
+}
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        int cnt=0;
+        vector<int>ans;
+        if(root==NULL) return ans;
+        if(k==0){
+            ans.push_back(target->val);
+            return ans;
+        }
+        unordered_map<TreeNode*, TreeNode*>parents;
+        markParents(root,parents);
+        dis(root,target,k,parents,ans);
+        
+       return ans;
+
+    }
+};
