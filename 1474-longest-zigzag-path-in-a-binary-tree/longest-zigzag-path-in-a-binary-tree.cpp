d5ccc88c7b02475bff12900360b6c8b3dafcea38
gudiDedeepya <gudidedeepya@gmail.com>
2026-06-01 18:35:55 +0530

    Time: 5 ms (38.62%) | Memory: 94.9 MB (71.34%) - LeetSync

---
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int maxi=0;
+    void zigzag(TreeNode* root,int state,int length){
+        if(root==NULL) return;
+        //root=0,left=1,right=2
+        if(state==0){
+            if(root->left){
+            maxi=max(maxi,length+1);
+            zigzag(root->left,1,length+1);
+            }
+            if(root->right){
+            maxi=max(maxi,length+1);
+            zigzag(root->right,2,length+1);
+            }
+
+        }
+        else if(state==2){
+            if(root->left){
+                maxi=max(maxi,length+1);
+                zigzag(root->left,1,length+1);
+            }
+                    maxi=max(maxi,length);
+                    zigzag(root->right,2,1);
+                
+        }
+        
+        else {
+            if(root->right){
+                maxi=max(maxi,length+1);
+                zigzag(root->right,2,length+1);
+                }
+                    maxi=max(maxi,length);
+                    zigzag(root->left,1,1);
+                
+        }
+    }
+    int longestZigZag(TreeNode* root) {
+        zigzag(root,0,0);
+        return maxi;
+    }
+};
