8beb4d1903eee8f0fbc576534c872fa5f1967ffc
gudiDedeepya <gudidedeepya@gmail.com>
2026-06-09 00:30:49 +0530

    Time: 7 ms (63.72%) | Memory: 12.9 MB (77.70%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,58 +1,43 @@
 class Solution {
 public:
 
-bool bfs(vector<vector<int>>& grid,int mid){
-        int t=mid;
-        vector<int>dr={-1,1,0,0};
-        int n=grid.size();
-        vector<int>dc={0,0,-1,1};
-        queue<pair<int,int>>q;
-        if(grid[0][0]<=t){
-        q.push({0,0});
-        }
-        //else return false;
-        vector<vector<int>>visited(n,vector<int>(n,0));
-        visited[0][0]=1;
-
-        while(!q.empty()){
-            auto [r,c]=q.front();
-            q.pop();
-            if(r==n-1&&c==n-1) return true;
-            for(int d=0;d<4;d++){
-                int nr=r+dr[d];
-                int nc=c+dc[d];
-                if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]<=t&&!visited[nr][nc]){
-                    q.push({nr,nc});
-                    visited[nr][nc]=1;
-                }
-            }
-
-        }
-        return false;
-
-}
     int swimInWater(vector<vector<int>>& grid) {
-        int maxi=0;
         int n=grid.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                maxi=max(maxi,grid[i][j]);
+         priority_queue<
+        pair<int,pair<int,int>>,
+        vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>
+        > pq;
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        pq.push({grid[0][0],{0,0}});
+        vis[0][0]=1;
+        int t=grid[0][0];
+               int dr[] = {-1,1,0,0};
+int dc[] = {0,0,-1,1};
+
+        while(!pq.empty()){
+            auto it=pq.top();
+            int time=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
+            pq.pop();
+            if(r==n-1&&c==n-1) return time;
+            t++;
+            for(int d=0;d<4;d++){
+                int nr = r + dr[d];
+               int nc = c + dc[d];
+
+                if(nr>=0&&nr<n&&nc>=0&&nc<n&&vis[nr][nc]!=1){
+                    vis[nr][nc]=1;
+                    if(grid[nr][nc]<=time)
+                    pq.push({time,{nr,nc}});
+                    else
+                    pq.push({grid[nr][nc],{nr,nc}});
             }
         }
-        int low=0;
-        int high=maxi;
-        int ans=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(bfs(grid,mid)){
-                high=mid-1;
-                ans=mid;
-            }
-            else{
-                low=mid+1;
-            }
+
         }
-        return ans;
+        return -1;
 
     }
 };
