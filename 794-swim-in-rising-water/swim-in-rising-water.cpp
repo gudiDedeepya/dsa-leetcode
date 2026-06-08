cfda6cd79de5a7b2a9b91ac57aef1bd750af788e
gudiDedeepya <gudidedeepya@gmail.com>
2026-06-09 00:51:32 +0530

    Time: 8 ms (51.03%) | Memory: 12.7 MB (86.00%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -8,13 +8,12 @@ public:
         vector<pair<int,pair<int,int>>>,
         greater<pair<int,pair<int,int>>>
         > pq;
-        vector<vector<int>>vis(n,vector<int>(n,0));
+        vector<vector<int>>vis(n,vector<int>(n,INT_MAX));
         pq.push({grid[0][0],{0,0}});
-        vis[0][0]=1;
+        vis[0][0]=grid[0][0];
         int t=grid[0][0];
                int dr[] = {-1,1,0,0};
 int dc[] = {0,0,-1,1};
-
         while(!pq.empty()){
             auto it=pq.top();
             int time=it.first;
@@ -22,17 +21,17 @@ int dc[] = {0,0,-1,1};
             int c=it.second.second;
             pq.pop();
             if(r==n-1&&c==n-1) return time;
-            t++;
             for(int d=0;d<4;d++){
                 int nr = r + dr[d];
                int nc = c + dc[d];
 
-                if(nr>=0&&nr<n&&nc>=0&&nc<n&&vis[nr][nc]!=1){
-                    vis[nr][nc]=1;
-                    if(grid[nr][nc]<=time)
-                    pq.push({time,{nr,nc}});
-                    else
-                    pq.push({grid[nr][nc],{nr,nc}});
+                if(nr>=0&&nr<n&&nc>=0&&nc<n){
+
+                    int newtime=max(grid[nr][nc],time);
+                    if(newtime<vis[nr][nc]){
+                        vis[nr][nc]=newtime;
+                    pq.push({newtime,{nr,nc}});
+                    }
             }
         }
 
