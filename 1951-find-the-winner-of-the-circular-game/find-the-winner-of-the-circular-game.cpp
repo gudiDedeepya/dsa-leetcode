151305ad251d808b9a7d3c36c813d9b198997120
gudiDedeepya <gudidedeepya@gmail.com>
2026-07-15 23:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.58%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+          int ans = 0; // f(1)
+
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + k) % i;
+        }
+
+        return ans + 1;
+
+
+        
+
+
+    }
+};
